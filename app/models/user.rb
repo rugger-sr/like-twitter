44f3a075bfc58239c6326d4786ebc2a34c2c400b
unknown <rugger.sr@gmail.com>
2018-04-15 17:05:56 +0900

    tab>space part4

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,13 +4,11 @@ class User < ApplicationRecord
   has_many :followers, autosave: true, :class_name => "Follow", :foreign_key => "target_user_id", :dependent => :destroy
 
   def is_follow?(user_id)
-  	self.follows.find_by(target_user_id: user_id).present?
+    self.follows.find_by(target_user_id: user_id).present?
   end
   
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
-
-
 end
