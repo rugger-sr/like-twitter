7c43674157cf8f363fe003edbe41ebed3a580b07
unknown <rugger.sr@gmail.com>
2018-04-15 16:41:06 +0900

    tab>space part2

--- config/routes.rb
+++ config/routes.rb
@@ -1,17 +1,17 @@
 Rails.application.routes.draw do
   get 'home/index'
-	root 'home#index'
+  root 'home#index'
 
   devise_for :users
   devise_scope :user do
     get '/users/sign_out' => 'devise/sessions#destroy'
   end
 
-	resources :users, only: [:show, :index] do
-	  resources :tweets, except: [:index]
+  resources :users, only: [:show, :index] do
+    resources :tweets, except: [:index]
     resources :follows, only: [:index, :create, :destroy]
     get :followers, to: 'follows#followers'
-	end
+  end
 #  get '/tweets' => 'tweets#tweet_list_partial'
   get '/search' => 'tweets#search_partial'
 end
